353419c63a6e16f40afeada2317086a0a9fe3f34
Tyler Mandry <tmandry@gmail.com>
2024-07-01 20:32:19 -0700

    Add devtool commands for interacting with apps

--- examples/devtool.rs
+++ examples/devtool.rs
@@ -3,13 +3,15 @@
 use std::{future::Future, time::Instant};
 
 use accessibility::{AXUIElement, AXUIElementAttributes};
+use accessibility_sys::pid_t;
+use anyhow::Context;
 use clap::{Parser, Subcommand};
 use core_foundation::{array::CFArray, base::TCFType, dictionary::CFDictionaryRef};
 use core_graphics::{
     display::{CGDisplayBounds, CGMainDisplayID},
     window::{
         kCGNullWindowID, kCGWindowListOptionExcludeDesktopElements,
-        kCGWindowListOptionOnScreenOnly, CGWindowListCopyWindowInfo,
+        kCGWindowListOptionOnScreenOnly, CGWindowID, CGWindowListCopyWindowInfo,
     },
 };
 use icrate::{
@@ -19,20 +21,31 @@ use icrate::{
 use nimbus_wm::{
     sys::app,
     sys::screen::{self, ScreenCache},
-    sys::window_server,
+    sys::window_server::{self, WindowServerId},
 };
 use tokio::sync::mpsc;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 #[derive(Parser)]
 struct Opt {
+    #[arg(long)]
     bundle: Option<String>,
     #[command(subcommand)]
-    command: Commands,
+    command: Command,
+    #[arg(long)]
+    verbose: bool,
 }
 
 #[derive(Subcommand, Clone)]
-enum Commands {
+enum Command {
+    #[command(subcommand)]
+    Show(Show),
+    #[command(subcommand)]
+    App(App),
+}
+
+#[derive(Subcommand, Clone)]
+enum Show {
     All,
     Apps,
     Ax,
@@ -41,8 +54,25 @@ enum Commands {
     Spaces,
 }
 
+#[derive(Subcommand, Clone)]
+enum App {
+    #[command()]
+    SetMainWindow {
+        pid: pid_t,
+        window_server_id: CGWindowID,
+        #[arg(long)]
+        wait: bool,
+    },
+    #[command()]
+    ReadMainWindow {
+        pid: pid_t,
+        #[arg(long)]
+        wait: bool,
+    },
+}
+
 #[tokio::main(flavor = "current_thread")]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     tracing_subscriber::registry()
         .with(EnvFilter::from_default_env())
         .with(tracing_tree::HierarchicalLayer::default())
@@ -50,11 +80,13 @@ async fn main() {
     let opt: Opt = Parser::parse();
 
     match opt.command {
-        Commands::Ax => time("accessibility", || get_windows_with_ax(&opt, false, true)).await,
-        Commands::Cg => time("core-graphics", || get_windows_with_cg(&opt, true)).await,
-        Commands::Ns => time("ns-window", || get_windows_with_ns(&opt, true)).await,
-        Commands::Apps => get_apps(&opt),
-        Commands::All => {
+        Command::Show(Show::Ax) => {
+            time("accessibility", || get_windows_with_ax(&opt, false, true)).await
+        }
+        Command::Show(Show::Cg) => time("core-graphics", || get_windows_with_cg(&opt, true)).await,
+        Command::Show(Show::Ns) => time("ns-window", || get_windows_with_ns(&opt, true)).await,
+        Command::Show(Show::Apps) => get_apps(&opt),
+        Command::Show(Show::All) => {
             //time("accessibility serial", || get_windows_with_ax(&opt, true)).await;
             time("core-graphics", || get_windows_with_cg(&opt, true)).await;
             time("ns-window", || get_windows_with_ns(&opt, true)).await;
@@ -69,7 +101,7 @@ async fn main() {
             })
             .await;
         }
-        Commands::Spaces => {
+        Command::Show(Show::Spaces) => {
             println!("Current space: {:?}", screen::diagnostic::cur_space());
             println!("Visible spaces: {:?}", screen::diagnostic::visible_spaces());
             println!("All spaces: {:?}", screen::diagnostic::all_spaces());
@@ -85,26 +117,75 @@ async fn main() {
             println!("Frames: {:?}", sc.update_screen_config());
             println!("Spaces: {:?}", sc.get_screen_spaces());
         }
+        Command::App(App::SetMainWindow { pid, window_server_id, wait }) => {
+            let app = AXUIElement::application(pid);
+            let windows = app.windows()?;
+            let window = windows
+                .iter()
+                .filter(|w| {
+                    let id: Result<window_server::WindowServerId, _> = (&**w).try_into();
+                    id.is_ok_and(|id| id.as_u32() == window_server_id)
+                })
+                .next()
+                .context("Could not find matching window")?;
+            if wait {
+                println!("Press enter to complete action");
+                std::io::stdin().read_line(&mut String::new())?;
+                window.set_messaging_timeout(3600.0)?;
+            }
+            window.set_main(true).context("Failed to set window as main")?;
+        }
+        Command::App(App::ReadMainWindow { pid, wait }) => {
+            let app = AXUIElement::application(pid);
+            println!("frontmost = {:?}", app.frontmost()?);
+            let main_window = if opt.verbose {
+                let main_window = dbg!(app.main_window()?);
+                let main_window_id: WindowServerId = (&app.main_window()?).try_into()?;
+                dbg!(main_window_id);
+                main_window
+            } else {
+                let main_window = app.main_window();
+                println!("main_window = {:?}", main_window.as_ref().map(|_| ()));
+                // let main_window_id: WindowServerId = (&app.main_window()?).try_into()?;
+                // dbg!(main_window_id);
+                main_window?
+            };
+            if wait {
+                println!("Press enter to complete action");
+                std::io::stdin().read_line(&mut String::new())?;
+                app.set_messaging_timeout(3600.0)?;
+            }
+            dbg!(main_window.main()?);
+        }
     }
+    Ok(())
 }
 
-async fn get_windows_with_cg(_opt: &Opt, print: bool) {
+async fn get_windows_with_cg(opt: &Opt, print: bool) {
     let windows: CFArray<CFDictionaryRef> = unsafe {
         CFArray::wrap_under_get_rule(CGWindowListCopyWindowInfo(
             kCGWindowListOptionOnScreenOnly | kCGWindowListOptionExcludeDesktopElements,
             kCGNullWindowID,
         ))
     };
-    if print {
+    if print && opt.verbose {
         println!("{windows:?}");
     }
-    println!(
-        "visible window ids = {:?}",
-        window_server::get_visible_windows()
-    );
+    if print {
+        println!("visible window ids:");
+        for window in window_server::get_visible_windows() {
+            if opt.verbose {
+                println!("- {window:?}");
+            } else {
+                println!("- {id:?}, pid={pid:?}", id = window.id, pid = window.pid);
+            }
+        }
+    }
     let display_id = unsafe { CGMainDisplayID() };
     let screen = unsafe { CGDisplayBounds(display_id) };
-    println!("main display = {screen:?}");
+    if print {
+        println!("main display = {screen:?}");
+    }
 }
 
 async fn get_windows_with_ns(_opt: &Opt, print: bool) {
