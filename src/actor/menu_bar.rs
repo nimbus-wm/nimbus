8a6c39ad52c441131fe94bf7c0bf6e37eec39698
Tyler Mandry <tmandry@gmail.com>
2025-07-14 19:23:47 -0700

    Add parachute to menu bar icon

--- src/actor/menu_bar.rs
+++ src/actor/menu_bar.rs
@@ -1,8 +1,9 @@
 //! Menu bar icon management for displaying the current space ID.
 
+use objc2::AnyThread;
 use objc2::rc::Retained;
-use objc2_app_kit::{NSStatusBar, NSStatusItem};
-use objc2_foundation::{MainThreadMarker, NSString};
+use objc2_app_kit::{NSImage, NSStatusBar, NSStatusItem};
+use objc2_foundation::{MainThreadMarker, NSData, NSString};
 use tracing::{debug, warn};
 
 use crate::sys::screen::SpaceId;
@@ -26,6 +27,13 @@ impl MenuBarManager {
         unsafe {
             let status_bar = NSStatusBar::systemStatusBar();
             let status_item = status_bar.statusItemWithLength(-1.0); // NSVariableStatusItemLength
+
+            // Create parachute icon
+            if let Some(parachute_image) = self.create_parachute_icon() {
+                if let Some(button) = status_item.button(self.mtm) {
+                    button.setImage(Some(&parachute_image));
+                }
+            }
 
             self.status_item = Some(status_item);
         }
@@ -68,6 +76,37 @@ impl MenuBarManager {
                 let status_bar = NSStatusBar::systemStatusBar();
                 status_bar.removeStatusItem(&status_item);
             }
+        }
+    }
+
+    /// Creates the parachute icon from the SVG file
+    fn create_parachute_icon(&self) -> Option<Retained<NSImage>> {
+        unsafe {
+            // Load the SVG file
+            let svg_data = include_str!("../../site/src/assets/parachute.svg");
+            let ns_data = NSData::dataWithBytes_length(
+                svg_data.as_ptr() as *const std::ffi::c_void,
+                svg_data.len(),
+            );
+
+            if let Some(image) = NSImage::initWithData(NSImage::alloc(), &ns_data) {
+                // Set the image size to be appropriate for menu bar (16x16 points)
+                image.setSize(objc2_core_foundation::CGSize { width: 16.0, height: 16.0 });
+                // Set as template image so it follows system appearance
+                image.setTemplate(true);
+                return Some(image);
+            }
+
+            // Fallback to SF Symbol if SVG loading fails
+            let symbol_string = NSString::from_str("parachute");
+            if let Some(image) =
+                NSImage::imageWithSystemSymbolName_accessibilityDescription(&symbol_string, None)
+            {
+                image.setTemplate(true);
+                return Some(image);
+            }
+
+            None
         }
     }
 }
